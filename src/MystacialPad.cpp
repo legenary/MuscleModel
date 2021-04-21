e200d98b60e7d0383749688105f970b90d0b8910
legenary <38062004+legenary@users.noreply.github.com>
2021-04-21 16:08:14 -0500

    change CMakeList: bullet3-master to bullet3

--- src/MystacialPad.cpp
+++ src/MystacialPad.cpp
@@ -207,10 +207,10 @@ void MystacialPad::debugDraw(btDiscreteDynamicsWorld* m_dynamicsWorld, int DEBUG
 		//}
 		//m_nasolabialis->debugDraw(m_dynamicsWorld, btVector3(1., 0., 0.));
 		//m_maxillolabialis->debugDraw(m_dynamicsWorld, btVector3(1., 0., 0.)); 
-		//m_NS->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
-		//m_PMS->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
-		//m_PMI->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
-		//m_PIP->debugDraw(m_dynamicsWorld, btVector3(0., 1., 0.));
+		m_NS->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
+		m_PMS->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
+		m_PMI->debugDraw(m_dynamicsWorld, btVector3(0., 0., 1.));
+		m_PIP->debugDraw(m_dynamicsWorld, btVector3(0., 1., 0.));
 		m_PM->debugDraw(m_dynamicsWorld, btVector3(0., 1., 0.));
 	}
 }
